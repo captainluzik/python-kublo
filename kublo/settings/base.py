d507850781be6ac503f5043f524fd4901c91e30a
dmytro <rungotika@gmail.com>
2023-08-17 17:07:29 +0200

    .env.example modified

--- kublo/settings/base.py
+++ kublo/settings/base.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0r4=%ogw%(90$c***#&64d+gye=s*dj335k1s-ee#8br#*qoye'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
