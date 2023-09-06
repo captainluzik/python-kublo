a95db6cba599d730a4c82bd8d07e0d3b3661db08
Reboot <sergejskorikjr@gmail.com>
2023-09-06 09:50:56 +0300

    Create personal account model

--- apps/cabinet_api/admin.py
+++ apps/cabinet_api/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from apps.cabinet_api.models import CustomUser
+from apps.cabinet_api.models import CustomUser, PersonalAccount
 
 
 @admin.register(CustomUser)
@@ -10,7 +10,18 @@ class CustomUserAdmin(UserAdmin):
     list_filter = ['is_staff', 'is_active', ]
     ordering = ['email', ]
     fieldsets = (
-        (None, {'fields': ('email', 'password', 'date_joined', )}),
+        (None, {'fields': ('email', 'password', 'date_joined',)}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
 
+
+@admin.register(PersonalAccount)
+class PersonalAccountAdmin(admin.ModelAdmin):
+    list_display = ('get_partners', 'deposit_term', 'total_deposit_amount', 'interest_rate', 'dividend_amount',)
+    list_filter = ('investment_sector', 'deposit_term',)
+    search_fields = ('partner_code', 'user__email', 'investment_sector',)
+
+    def get_partners(self, obj):
+        return ", ".join([partner.email for partner in obj.partners.all()])
+
+    get_partners.short_description = 'Partners'
--- apps/cabinet_api/models.py
+++ apps/cabinet_api/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
+from django.core import validators
 from django.db import models
+
+from core import settings
 
 
 class CustomUserManager(BaseUserManager):
@@ -35,3 +38,35 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return f'{self.email}'
+
+
+class PersonalAccount(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='account', on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255, validators=[
+        validators.MinLengthValidator(2, message="First name should be at least 2 characters long"),
+    ])
+    last_name = models.CharField(max_length=255, validators=[
+        validators.MinLengthValidator(2, message="Last name should be at least 2 characters long"),
+    ])
+    partner_code = models.CharField(max_length=10, unique=True)
+    investment_sector = models.CharField(max_length=50, validators=[
+        validators.MinLengthValidator(2, message="Investment sector should be at least 2 characters long"),
+    ])
+    deposit_term = models.DurationField()
+    total_deposit_amount = models.DecimalField(max_digits=20, decimal_places=10)
+    interest_rate = models.DecimalField(max_digits=5, decimal_places=2)
+    dividend_amount = models.DecimalField(max_digits=20, decimal_places=10)
+
+    partners = models.ManyToManyField(CustomUser, related_name='partner', blank=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=('partner_code',))
+        ]
+
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return f'{self.user.email} - {self.investment_sector}'
